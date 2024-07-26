0e7617fc7e85911b1180487a706bad66c2a3816d
kbwhodat <byantalok@yahoo.com>
2024-07-26 00:53:40 -0400

    using curl to obtain artifact

--- overlay.nix
+++ overlay.nix
@@ -1,43 +1,38 @@
 self: super:
 {
-  ghostty-darwin = edition:
-    super.stdenv.mkDerivation rec {
+  ghostty-darwin = super.stdenv.mkDerivation rec {
       pname = "Ghostty";
-  
-      buildInputs = [ super.pkgs.unzip ];
+      version = "tip";
+
+      buildInputs = [ super.unzip super.curl ];
       sourceRoot = ".";
+
       phases = [ "unpackPhase" "installPhase" ];
-  
+
       unpackPhase = ''
         runHook preUnpack
-        unzip  "$src" -d "$sourceRoot"
+        curl -L -H "Accept: application/octet-stream" \
+             -H "Authorization: Bearer ${builtins.getEnv "GITHUB_TOKEN"}" \
+             -H "X-GitHub-Api-Version: 2022-11-28" \
+             https://api.github.com/repos/ghostty-org/ghostty/releases/assets/181797263 \
+             --output ghostty.zip -k
+        unzip ghostty.zip -d "$sourceRoot"
         runHook postUnpack
       '';
 
       installPhase = ''
         runHook preInstall
-    
+
         mkdir -p $out/Applications
         cp -r Ghostty.app "$out/Applications/"
-    
+
         runHook postInstall
       '';
 
-# If this doesn't work this, it because you do not have access to the repo
-      src = super.fetchurl {
-        url = "https://api.github.com/repos/ghostty-org/ghostty/releases/assets/181797263";
-        sha256 = super.lib.fake256Sha; 
-        headers = [
-          "Accept: application/octet-stream"
-            (builtins.concatStringsSep " " ["Authorization: Bearer" (builtins.getEnv "GITHUB_TOKEN")])
-            "X-GitHub-Api-Version: 2022-11-28"
-        ];
-      };
-  
       meta = {
         description = "Ghostty is a cross-platform, GPU-accelerated terminal emulator.";
         homepage = "https://mitchellh.com/ghostty";
+        license = super.lib.licenses.mit;
       };
     };
-
 }
