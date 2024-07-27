de1b537b6c03f008fff577e778a33fbfdaef6df0
kbwhodat <byantalok@yahoo.com>
2024-07-27 06:08:27 -0400

    using variable in ci-shell

--- ci-shell.nix
+++ ci-shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; } }:
+{ githubToken, pkgs ? import <nixpkgs> { overlays = [ (import ./overlay.nix { inherit githubToken; } ) ]; } }:
 pkgs.mkShell {
   packages = with pkgs; [
     ghostty-darwin
